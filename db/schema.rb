a936ed9f965a100ea2a4a11d5566a3a19d9c2fae
Lisiane Fonteneau <lisiane.fonteneau@live.fr>
2018-08-07 11:35:09 +0200

    Add completed to Tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_07_092740) do
+ActiveRecord::Schema.define(version: 2018_08_07_093208) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed", default: true
   end
 
 end
